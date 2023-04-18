cafe815636531553664e92f218d835dcc447b6f7
Kev Zettler <kevzettler@gmail.com>
2023-04-17 22:43:56 -0700

    Reproduce landon export dependencies
    add signalhub script to npm dev script
    add animations:build script to npm scripts

--- scripts/landon_export_actions.sh
+++ scripts/landon_export_actions.sh
@@ -1,3 +1,23 @@
-RUST_BACKTRACE=1 landon blender export -f ./assets/WiP-punch.blend > landon_export &&
+# this requires a blender file that was last saved in blender version 2.80^
+# This depends on blender being installed on command line
+
+#This is dependent on landon v0.3.6
+# https://github.com/chinedufn/landon/commit/3812c4fd4f2ba8c085dbdc41aa9ce5f7594dd29e
+
+# $ cargo install landon@0.3.6
+
+# landon has 3 other dependencies that are manually managed
+#
+# Install blender mesh json exporter
+# $ landon blender install mesh-to-json
+
+# Install blender armature json addon
+# $ landon blender install armature-to-json
+
+# https://github.com/chinedufn/landon/issues/3#issuecomment-531479000
+# $ npm install -g blender-iks-to-fks@2.0.0
+# $ ik2fk --install
+
+RUST_BACKTRACE=full landon blender export -f ./assets/WiP-punch.blend > landon_export &&
 cat landon_export | sed -n '/START_ARMATURE_JSON/,/END_ARMATURE_JSON/{/START_ARMATURE_JSON/!{/END_ARMATURE_JSON/!p;};}' &&
     rm landon_export;
